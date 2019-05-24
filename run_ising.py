beb1ddd15d5934c856c15370aae448fc9d31fc6a
SemyonSinchenko <semyonsinchenko@yahoo.com>
2019-05-24 22:13:01 +0300

    Updates for Lomonosov1

--- run_ising.py
+++ run_ising.py
@@ -1,17 +1,18 @@
 from python.src.Ising import Ising
 from python.src.Report import save_results
+import numpy as np
 
 # PARAMS
-N_SPINS = [4, 6, 10, 20, 40, 80, 160, 320]
+H_RANGE = list(range(2, 100)) + np.linspace(0.001, 1, 99).tolist()
+N_SPINS = [4, 6, 10, 20, 40]
 JZ_CONST = 1
-H_CONST = [JZ_CONST * coef
-           for coef in (list(range(1, 300)) + [x / 300 for x in range(1, 300)])]
+H_CONST = [JZ_CONST * coef for coef in H_RANGE]
 
 if __name__ == "__main__":
     for spins in N_SPINS:
         for h in H_CONST:
             model = Ising(n_spins=spins, J=JZ_CONST, h=h)
-            model.fit("output", max([spins * 12, 100]))
+            model.fit("output", 400)
             save_results("output" + '.log',
                          params=[spins, JZ_CONST, h],
                          prefix="ising", outfile="IsingResultsFull.csv")
