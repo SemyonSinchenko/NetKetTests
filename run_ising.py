a4c5a64ae3342f681c24c06c43525104060b0b8c
SemyonSinchenko <semyonsinchenko@yahoo.com>
2019-06-03 12:56:40 +0300

    Fixes

--- run_ising.py
+++ run_ising.py
@@ -3,7 +3,7 @@ from python.src.Report import save_results
 import numpy as np
 
 # PARAMS
-H_RANGE = list(range(2, 100, 4)) + np.linspace(0.001, 1, 25).tolist()
+H_RANGE = list(range(2, 100, 5)) + np.linspace(0.001, 1, 20).tolist()
 N_SPINS = [4, 6, 8, 10, 12, 14, 16, 18]
 JZ_CONST = 1
 H_CONST = [JZ_CONST * coef for coef in H_RANGE]
@@ -11,10 +11,12 @@ H_CONST = [JZ_CONST * coef for coef in H_RANGE]
 if __name__ == "__main__":
     for spins in N_SPINS:
         for h in H_CONST:
-            for i in range(3):
+            exact = -1
+            for i in range(2):
                 model = Ising(n_spins=spins, J=JZ_CONST, h=h)
                 model.fit("output", 400)
-                exact = model.get_exact()
+                if i==0:
+                    exact = model.get_exact()
                 save_results("output" + '.log',
                              prefix="ising",
                              num_iter=i,
